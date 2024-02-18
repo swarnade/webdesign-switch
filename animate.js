2b80084f4ccb78bbcd2573cc4db4b1203befc4e8
SOHAN <sohanroychowdhury7@gmail.com>
2024-02-18 22:52:15 +0530

    Added Loading Page with logo

---
+++ animate.js
@@ -0,0 +1,29 @@
+let boxes = document.querySelectorAll(".box-main");
+
+console.log(boxes);
+Array.from(boxes).forEach((e)=> {
+    e.classList.add("hide");
+})
+
+let footer = document.querySelector("footer");
+footer.classList.add("hide");
+
+let nav = document.querySelector("nav");
+nav.classList.add("hide");
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load" , function () {
+    loader.style.display = "none";
+
+    setTimeout(() => {
+        Array.from(boxes).forEach((e)=> {
+            e.classList.remove("hide");
+        })
+    
+        footer.classList.remove("hide");
+        nav.classList.remove("hide");
+    }, 5000);
+
+
+})
